f5d353024f1164da0d21e586949e3ffac006b3ed
Your Name <you@example.com>
2014-12-18 10:26:49 +0200

    last version

--- app/assets/javascripts/list_view_click.js
+++ app/assets/javascripts/list_view_click.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   $('.show-list').click(function() {
   $( ".school-list" ).show();
-  $( ".search-map" ).hide();
+  $( ".events-wrapper" ).show();
+  $( ".map" ).hide();
   $( ".show-list" ).addClass('active');
   $( ".show-map" ).removeClass('active');
   $( ".pagination" ).removeClass('none');
   });
   $('.show-map').click(function() {
   $( ".school-list" ).hide();
-  $( ".search-map" ).show();
+  $( ".events-wrapper" ).hide();
+  $( ".map" ).show();
   $( ".show-map" ).addClass('active');
   $( ".show-list" ).removeClass('active');
   $( ".pagination" ).addClass('none');
